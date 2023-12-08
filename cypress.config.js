de003f85044753d1e27ec985ddd78e28f5dc9e22
EyadMuhamed <116434993+EyadMuhamed@users.noreply.github.com>
2023-12-08 20:23:11 +0200

    update delete account

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',   //for HTML reports
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
